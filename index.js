6159d1283d77b7adb99035af34dd0b26545a71e4
hasanfoysal <hasanfoysal178@gmail.com>
2024-07-06 13:58:35 +0600

    lala

---
+++ index.js
@@ -0,0 +1,152 @@
+
+const express = require('express');
+const cors = require('cors');
+var jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uivgn73.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uivgn73.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const userCollection = client.db('productsHunt').collection('users')
+    const productsCollection = client.db('productsHunt').collection('services')
+    const tagCollection = client.db('productsHunt').collection('tag')
+    const trendingCollection = client.db('productsHunt').collection('trending')
+
+
+ 
+    app.post('/jwt', async (req, res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCES_TOKEN_SECRET, {
+        expiresIn: '1hr'
+      })
+      res.send({ token });
+    })
+
+    const verifyToken = (req, res, next) =>{
+      console.log( 'inside verify token' , req.headers.authorization);
+      // next();
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'forbidden access'});
+      }
+      const token = req.headers.authorization.split('')[1]
+      jwt.verify(token, process.env.ACCES_TOKEN_SECRET, (err, decoded) =>{
+        if(err){
+          return res.status(401).send({message: 'forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+      })
+
+    }  
+
+    app.post('/users',async (req, res)=>{
+        const user = req.body;
+        const query = {email:user.email}
+        const existingUser = await userCollection.findOne(query);
+        if(existingUser){
+        return res.send({message: 'user already exists', insertedId:null})
+        }
+        const result = await userCollection.insertOne(user);
+        res.send(result)
+    })
+
+    app.patch('/users/admin/:id', async(req, res) =>{
+      const id =req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    })
+
+    app.delete('/users/:id', async(req,res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+      })
+
+    app.get('/users',   async(req, res)=>{
+      console.log(req.headers)
+        const result = await userCollection.find().toArray();
+        res.send(result);
+    })
+
+    
+    app.get('/services', async(req, res) =>{
+        const cursor = productsCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    
+    app.get('/trending', async(req, res) =>{
+      const filter = req.query;
+      console.log(filter) ;
+      const query = {};
+      const options = {
+        sort: {
+          vote_count: filter.sort === 'asc' ? 1: -1
+        }
+      };
+
+        const cursor = trendingCollection.find(query, options);
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+   
+    app.get('/tag', async(req, res) =>{
+        const cursor = tagCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
